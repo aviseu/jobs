b52a34b59db57c3c55e496417f74cd2ef1ebe3b8
aviseu <alexandre@viseu.me>
2025-04-29 19:40:10 +0200

    adding grpc

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use AV\ProtoBuf\Events\Jobs\JobInformation;
 
 class ExampleTest extends TestCase
 {
@@ -15,6 +16,8 @@ class ExampleTest extends TestCase
     public function test_the_application_returns_a_successful_response()
     {
         $response = $this->get('/');
+
+        $info = new JobInformation();
 
         $response->assertStatus(200);
     }
